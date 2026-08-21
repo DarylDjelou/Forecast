e7eb1df4ac208c856f9f43c6c64488b3100cb305
Djelou Daryl <daryl.djelou@epitech.eu>
2026-08-21 22:49:25 +0200

    feat: add geocoding for cities name

---
+++ src/ingestion/geocoding.py
@@ -0,0 +1,31 @@
+from geopy.geocoders import Nominatim
+
+
+geolocator = Nominatim(user_agent="Forecast")
+
+def geocode_city(city_name):
+    """
+    Geocode a city name to get its latitude and longitude.
+
+    Args:
+        city_name (str): The name of the city to geocode.
+    Returns:
+        dict: A dictionary containing the city name, latitude, longitude, and display name if found, otherwise None.
+    """
+
+    if not isinstance(city_name, str) or not city_name.strip():
+        raise ValueError("city must be a non-empty string")
+    try:
+        location = geolocator.geocode(city_name)
+    except Exception as e:
+        print(f"Error geocoding city '{city_name}': {e}")
+        return None
+    if location:
+        return {
+            "city": city_name,
+            "latitude": location.latitude,
+            "longitude": location.longitude,
+            "display_name": location.address
+        }
+    else:
+        return None
